5b331639e2c2769bb18b2b76439425af9556faf7
Daniel Samson Raj <danielsam458@gmail.com>
2020-05-23 01:30:09 +0530

    added language translation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from twilio.rest import Client
 import requests
 import random
+from translate import Translator
 
 account_sid = 'AC68981bf65fee38f68a59a56eac6b7486'
 auth_token = '4b7cabfb977462f9211d8072c64030f2'
@@ -15,11 +16,14 @@ verse = requests.get('https://bible-api.com/' +
 
 current_message = verse.json()['text']
 
+translator = Translator(to_lang="Telugu")
+translated_message = translator.translate(current_message)
+
 
 def send_msg():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
-        body=current_message,
+        body=current_message + '\n' + translated_message,
         to='whatsapp:+918309282168'
     )
 
