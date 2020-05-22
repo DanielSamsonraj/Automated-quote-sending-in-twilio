c61326407ae9d4697066d95f0036b9e6c509cc9d
Daniel Samson Raj <danielsam458@gmail.com>
2020-05-23 01:21:41 +0530

    intital commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from twilio.rest import Client
+import requests
+import random
+
+account_sid = 'AC68981bf65fee38f68a59a56eac6b7486'
+auth_token = '4b7cabfb977462f9211d8072c64030f2'
+client = Client(account_sid, auth_token)
+
+book_name = 'john '
+chapter_no = '1'
+verse_no = '1'
+
+verse = requests.get('https://bible-api.com/' +
+                     book_name + chapter_no + ':' + verse_no)
+
+current_message = verse.json()['text']
+
+
+def send_msg():
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=current_message,
+        to='whatsapp:+918309282168'
+    )
+
+    print(message.sid)
---
+++ clock.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from app import send_msg
+
+sched = BlockingScheduler()
+
+# Schedule job_function to be called every two hours
+sched.add_job(send_msg, 'interval', seconds=2)
+
+sched.start()
